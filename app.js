462fb582142c575d877a4f96147c7782b1269d7f
shafi <shafimiya7679@gmail.com>
2025-07-14 03:59:42 +1000

    feat: created first functional component

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', {id: "heading"}, 'Hello, React!');
+const Title = () => {
+    return <h2>this is a title document</h2>
+}
+
+const HeadingComponent = () => {
+    return (
+        <div>
+            <Title />
+            <h1 className='heading'>here is a functional component</h1>
+        </div>
+)
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<HeadingComponent />);
